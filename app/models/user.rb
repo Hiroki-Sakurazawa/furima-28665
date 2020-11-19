f55102055cd461c1b8197036254a4a3478fd47d3
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2020-11-19 17:24:01 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: {minimum:6}
+  validates :surname1, presence: true
+  validates :first_name1, presence: true
+  validates :surname2, presence: true
+  validates :first_name2, presence: true
+  validates :dob, presence: true
 end
