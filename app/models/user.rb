133e0be1a3621de9b3c66e61b63270956cd1eac7
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2020-11-21 02:56:41 +0900

    rubocop実行・修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i  }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :surname1, presence: true, format: { with: /\A[ぁ-ん一-龥]+\z/ }
   validates :first_name1, presence: true, format: { with: /\A[ぁ-ん一-龥]+\z/ }
   validates :surname2, presence: true, format: { with: /\A[ァ-ン]+\z/ }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
-    email                 {"test@test"}
-    password              {"test12"}
-    password_confirmation {password}
-    surname1              {"てすと"}
-    first_name1              {"てすと"}
-    surname2              {"テスト"}
-    first_name2              {"テスト"}
-    dob              {"9999-12-31"}
+    nickname              { 'test' }
+    email                 { 'test@test' }
+    password              { 'test12' }
+    password_confirmation { password }
+    surname1              { 'てすと' }
+    first_name1 { 'てすと' }
+    surname2 { 'テスト' }
+    first_name2 { 'テスト' }
+    dob { '9999-12-31' }
   end
 end
