2e346891feb24cb59fda7128ef442c368bf5b0ba
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2020-11-23 04:39:53 +0900

    ActiveHash導入・各モデル作成・アソシエーション・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :area
+  belongs_to :days
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :status_id, numericality: { other_than: 1 }
+    validates :fee_id, numericality: { other_than: 1 }
+    validates :area_id, numericality: { other_than: 1 }
+    validates :days_id, numericality: { other_than: 1 }
+    validates :price
+  end
 end
