c6d57e4e5abe3ef435c37d2c0f6e30181ffc8c7a
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2020-11-23 01:46:09 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,13 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :fee_id, :arei_id, :days_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
