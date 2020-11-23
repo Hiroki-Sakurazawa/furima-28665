52beaab3cf04b6aa3eaedcacd6f8cd3d9a37fa2e
Hiroki-Sakurazawa <hiroki.12.dec@hotmail.co.jp>
2020-11-23 19:59:04 +0900

    商品登録画面エラー表示実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,17 @@ class Item < ApplicationRecord
   belongs_to :fee
   belongs_to :area
   belongs_to :day
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :fee_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :days_id, numericality: { other_than: 1 }
+
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id, numericality: { other_than: 1 }
-    validates :status_id, numericality: { other_than: 1 }
-    validates :fee_id, numericality: { other_than: 1 }
-    validates :area_id, numericality: { other_than: 1 }
-    validates :days_id, numericality: { other_than: 1 }
     validates :price
+    validates :image
   end
 end
